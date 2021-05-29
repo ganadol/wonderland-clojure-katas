ba8e98bc28d919dea48d2262414b5b7f6f25acf6
junsup so <night1700@junsupui-iMac.local>
2021-05-29 17:10:50 +0900

    #2 wonderland-number 구현

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,19 @@
 (ns wonderland-number.finder)
 
+(def wonderland-number-range (range 100000 1000000 1))
+
+(defn hasAllTheSameDigits [n1 n2]
+  (let [s1 (set (str n1))
+        s2 (set (str n2))]
+    (= s1 s2)))
+
+(defn is-wonderland-number? [n]
+  (if (= 6 (count (str n)))
+    (every? #(hasAllTheSameDigits n (* % n)) '(2 3 4 5 6))
+    false))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (loop [l wonderland-number-range]
+    (if (is-wonderland-number? (first l))
+      (first l)
+      (recur (rest l)))))
