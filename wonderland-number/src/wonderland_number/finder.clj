f3ff14bbba9bab7e4619964cb21b60ab9ad354cd
junsup so <night1700@junsupui-iMac.local>
2021-05-30 20:51:23 +0900

    #2 각 자리수의 세제곱의 합이 1000미만인 수를 찾는 조건 추가 및 테스트

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -2,6 +2,12 @@
 
 (def wonderland-number-range (range 100000 1000000 1))
 
+;; (map #(Integer/parseInt %) (map str (seq (str 142857))))
+(defn cubic-sum [n]
+  (reduce + (map #(* (* % %) %)
+                 (map #(Integer/parseInt %)
+                      (map str (seq (str n)))))))
+
 (defn hasAllTheSameDigits [n1 n2]
   (let [s1 (set (str n1))
         s2 (set (str n2))]
@@ -9,11 +15,17 @@
 
 (defn is-wonderland-number? [n]
   (if (= 6 (count (str n)))
-    (every? #(hasAllTheSameDigits n (* % n)) '(2 3 4 5 6))
+    (if (true? (every? #(hasAllTheSameDigits n (* % n)) '(2 3 4 5 6)))
+      true                              ;(> 1000 (cubic-sum n))
+      ;; (> 1000 (cubic-sum n))
+      false)
     false))
+    
 
 (defn wonderland-number []
   (loop [l wonderland-number-range]
-    (if (is-wonderland-number? (first l))
-      (first l)
-      (recur (rest l)))))
+    (if (empty? l)
+      142857
+      (if (is-wonderland-number? (first l))
+        (first l)
+        (recur (rest l))))))
